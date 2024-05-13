d22749be1730eeff43e281752ee49b43a97656ad
josh <8550998+josh-Fen@users.noreply.github.com>
2024-05-13 00:04:10 -0400

    Style html

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let first = null;
+let second = null;
+let operator = null;
+
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +17,22 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(first, operator, second) {
+    switch (operator) {
+        case '+':
+            add(first, second);
+            break;
+        case '-':
+            subtract(first, second);
+            break;
+        case '*':
+            multiply(first, second);
+            break;
+        case '/':
+            divide(first, second);
+            break;
+
+    }
 }
