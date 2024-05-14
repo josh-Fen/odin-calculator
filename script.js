313c6726381500debc80dfe8f69c5a57f414e5e6
josh <8550998+josh-Fen@users.noreply.github.com>
2024-05-13 22:24:17 -0400

    Wire up display and add display functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let first = null;
 let second = null;
 let operator = null;
+let displayNum = '';
 
 
 function add(a, b) {
@@ -21,18 +22,70 @@ function divide(a, b) {
 
 function operate(first, operator, second) {
     switch (operator) {
-        case '+':
+        case 'add':
             add(first, second);
             break;
-        case '-':
+        case 'subtract':
             subtract(first, second);
             break;
-        case '*':
+        case 'multiply':
             multiply(first, second);
             break;
-        case '/':
+        case 'divide':
             divide(first, second);
             break;
 
     }
 }
+
+const display = document.querySelector('.result');
+const numbers = document.querySelectorAll('.num');
+const functions = document.querySelectorAll('.func');
+const clear = document.querySelector('.clear');
+const equals = document.querySelector('.equals');
+
+
+numbers.forEach((num) => {
+    num.addEventListener('click', (e) => {
+        //console.log(e.currentTarget.textContent);
+        if (displayNum.length < 10) {
+            displayNum += e.currentTarget.textContent;
+        }
+        console.log(`n is equal to ${displayNum}`);
+        displayNumber(displayNum);
+    });
+});
+
+function displayNumber(str) {
+    if (str.length <= 10) {
+        display.textContent = str;
+    } else {
+        display.textContent = str.slice(0, 10);
+    }
+}
+
+clear.addEventListener('click', (e) => {
+    displayNum = '';
+    first = '';
+    second = '';
+    operator = null;
+    displayNumber(displayNum);
+});
+
+functions.forEach((func) => {
+    func.addEventListener('click', (e) => {
+        operator = e.currentTarget.id;
+        if (first) {
+            second = displayNum;
+        } else {
+            first = displayNum;
+        }
+        displayNum = '';
+        console.log(`First is ${first} and Second is ${second}`);
+    });
+});
+
+equals.addEventListener('click', (e) => {
+    console.log('check');
+});
+
